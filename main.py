11dbdf556ef4f9f096be9ed8f1c6f7e6dbcf1183
Teacher NT <alpholmon@gmail.com>
2026-09-05 10:02:12 +0500

    tuple

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import os
 os.system("cls")
 
-tuple1 = ("Islom", 20, 1.75, "Toshknet")
+# tuple1 = ("Islom", 20, 1.75, "Toshknet")
 
-# tuple1[2] = 1.90 error
-print(tuple1)
-print(tuple1[2])
+# # tuple1[2] = 1.90 error
+# print(tuple1)
+# print(tuple1[2])
+
+tuple2 = (1,2,3,4,5,6,7,8,9)
+
+print(tuple2[4])
+print(tuple2[3:])
+print(tuple2[:3])
+print(tuple2[3:6])
+print(tuple2[2:8:2])
+print(tuple2[-2])
+print(tuple2[::-1])

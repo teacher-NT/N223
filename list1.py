c29bde70bac0a319711ccb2f17331a4ed153937a
Teacher NT <alpholmon@gmail.com>
2026-09-05 11:11:18 +0500

    tuple

---
+++ list1.py
@@ -0,0 +1,15 @@
+import os
+os.system("cls")
+
+# list1 = ['Toshkent', 'Samarqand', 'Buxoro', 'Andijon']
+# list1[1] = 'Namangan'
+# print(list1)
+# print(list1[2])
+
+
+tuple1 = ('e', 'a','b','c','e', 1,2,3,4,5, 'a','b','e')
+# print(tuple1.index('e'))
+# print(tuple1.index(2))
+# print(tuple1.index('salom')) error
+
+print(tuple1.count('e'))

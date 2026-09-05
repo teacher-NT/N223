11cda1fe9a517132abb6f5b2850b7ace5b8f7942
Teacher NT <alpholmon@gmail.com>
2026-09-05 11:29:51 +0500

    tuple

--- list1.py
+++ list1.py
@@ -7,9 +7,53 @@ os.system("cls")
 # print(list1[2])
 
 
-tuple1 = ('e', 'a','b','c','e', 1,2,3,4,5, 'a','b','e')
+# tuple1 = ('e', 'a','b','c','e', 1,2,3,4,5, 'a','b','e')
 # print(tuple1.index('e'))
 # print(tuple1.index(2))
 # print(tuple1.index('salom')) error
 
-print(tuple1.count('e'))
+# print(tuple1.count('e'))
+
+# ===============================================
+# LIST METODLARI
+
+names = ['Aziz', 'Laylo', 'Bunyod', 'Dilbar']
+# names.append('Ilhom')
+# print(names)
+
+# names.insert(2, 'Ilhom')
+# print(names)
+
+names2 = ['Ali', 'Murod', 'Javohir']
+# names.extend(names2)
+# names += names2
+# print(names)
+
+# names.remove('Bunyod')
+# print(names)
+
+# a = names.pop(1)
+# print(names)
+# print(a)
+
+# names.clear()
+# print(names)
+
+# names.sort()
+# print(names)
+
+# nums = [2,8,3,4,5,1,3,7]
+# nums.sort(reverse=True)
+# print(nums)
+
+# ismlar = names.copy()
+# ismlar.append('Dilshod')
+# print(ismlar)
+# print(names)
+
+# ismlar2 = names
+# ismlar2.append('Shohruh')
+# print(ismlar2)
+# print(names)
+
+

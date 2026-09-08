da8840ff41a7d483ab83f8f7d41fd01f81b96ef9
Teacher NT <alpholmon@gmail.com>
2026-09-08 10:11:27 +0500

    set methods 1

---
+++ set1.py
@@ -0,0 +1,56 @@
+import os
+os.system("cls")
+
+# set1 = {90,20,34,50,16,2,20,50,16}
+# print(set1)
+# print(set1[2]) error
+# set1[2] = 25 error
+# ===================================================
+
+# myset = {55,44,66,33,77,22,88}
+# for i in myset:
+#     print(i, end=" ")
+
+# fruits = {"olma", 'anor', 'gilos', 'banan', 'shaftoli'}
+# print()
+
+# if 'anor' in fruits:
+#     print("Bor")
+# else:
+#     print("Yo'q")
+# =====================================================
+
+
+myset2 = {90,80,70,60,50}
+
+# myset2.add(10)
+# print(myset2)
+
+# myset2.remove(60)
+# print(myset2)
+
+# myset2.discard(20)
+# print(myset2)
+
+# n = myset2.pop()
+# print(myset2)
+# print(n)
+
+# myset2.clear()
+# print(myset2)
+
+# myset3 = myset2.copy()
+# print(myset2)
+# print(myset3)
+
+# mylist = [1,10,20,30,60,50]
+# myset2.update(mylist)
+# print(myset2)
+
+# mylist = [1,10,20,30,60,50]
+# myset3 = myset2.union(mylist)
+# print(myset2)
+# print(myset3)
+
+
+

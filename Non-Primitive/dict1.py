ebd08595f1b6203dce3527120f61ae008de0ceee
Teacher NT <alpholmon@gmail.com>
2026-09-10 09:17:54 +0500

    dict1

---
+++ Non-Primitive/dict1.py
@@ -0,0 +1,19 @@
+import os
+os.system("cls")
+
+car = {
+    "brand": "BMW",
+    "model": "M3",
+    "price": 120_000,
+    "color": "blue",
+    "milage": 50_000,
+    "model": "Damas"
+}
+
+car['year'] = 2022
+
+# car["model"] = "M5 Compitetion"
+print(car)
+print(car['brand'])
+print(car['color'])
+print(car['model'])

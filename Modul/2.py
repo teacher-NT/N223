e21cb026a2b70ec5e888ee99c3fdc4b1ad929288
Teacher NT <alpholmon@gmail.com>
2026-09-17 09:51:48 +0500

    Math

---
+++ Modul/2.py
@@ -0,0 +1,20 @@
+import os
+os.system("cls")
+
+import math
+
+print(math.pow(3, 4)) # 3**4
+
+print(math.sqrt(25)) # 25**0.5
+
+print(math.log2(32))
+print(math.log10(100000))
+print(math.log(8, 2))
+
+print(math.floor(4.3))
+print(math.floor(4.8))
+
+print(math.ceil(4.3))
+print(math.ceil(4.5))
+
+print(round(4.3), round(4.7))

55e5d92ba39d02fc02e62d2b0f1631a5262f3426
Teacher NT <alpholmon@gmail.com>
2026-09-19 09:56:24 +0500

    file write

--- File/2.py
+++ File/2.py
@@ -1,13 +1,22 @@
 import os
 os.system("cls")
 
-file = open("myfile.txt", "w")
+# file = open("myfile.txt", "w")
 
 # text = input("Matn kiriting: ")
 # file.write(text)
 # print("Fayl yozildi")
 
-ismlar = ['ali', 'vali', 'hasan', 'husan']
-ismlar = list(map(lambda n: n+"\n", ismlar))
-file.writelines(ismlar)
-print("faylga yozildi")
+# ismlar = ['ali', 'vali', 'hasan', 'husan']
+# ismlar = list(map(lambda n: n+"\n", ismlar))
+# file.writelines(ismlar)
+# print("faylga yozildi")
+# file.close()
+
+# =========================================================
+
+with open("myfile.txt", "w") as file:    
+    text = input("Matn kiriting: ")
+    file.write(text)
+    print("Fayl yozildi")
+
